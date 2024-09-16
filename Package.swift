12df97f4cac6b4cd9b5cd71a43d6c9938906f385
Prismik <fbp@NyxPad.localdomain>
2024-09-15 22:52:06 -0400

    Update swift version and package to SPM 5.10

--- Package.swift
+++ Package.swift
@@ -5,12 +5,23 @@ import PackageDescription
 let package = Package(
     name: "Nifty",
     products: [
-        .library(name: "nifty", targets: ["nifty"])
+        .library(name: "Nifty", targets: ["Nifty"])
     ],
     dependencies: [
         .package(url: "git@github.com:Prismik/Nifty-libs.git", branch: "master"),
     ],
     targets: [
-        .target(name: "nifty")
+        .target(
+            name: "Nifty",
+            dependencies: [
+                .product(name: "CLapacke", package: "Nifty-libs"),
+                .product(name: "CBlas", package: "Nifty-libs"),
+            ]
+        ),
+        .testTarget(
+            name: "NiftyTests",
+            dependencies: ["Nifty"],
+            path: "Tests"
+        )
     ]
 )
